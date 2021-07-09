95f18bfa93596d35f088d11d0f5d4e49aba5ca88
Leo Gaskin <leo.gaskin@le0.gs>
2021-07-10 01:07:42 +0200

    Use data instead of resource for Cloudflare zone

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,7 +1,8 @@
-resource "cloudflare_zone" "digraph_me" {
-  zone = "digraph.me"
-  type = "full"
-  plan = "free"
+data "cloudflare_zones" "digraph_me" {
+  filter {
+    name = "digraph.me"
+    status = "active"
+  }
 }
 
 resource "cloudflare_record" "digraph_me" {
@@ -9,8 +10,8 @@ resource "cloudflare_record" "digraph_me" {
   proxied = true
   ttl     = 1
   type    = "A"
-  zone_id = cloudflare_zone.digraph_me.id
   value   = hcloud_server.webserver.ipv4_address
+  zone_id = data.cloudflare_zones.digraph_me.zones[0].id
 }
 
 resource "cloudflare_record" "raw_digraph_me" {
@@ -18,6 +19,6 @@ resource "cloudflare_record" "raw_digraph_me" {
   proxied = false
   ttl     = 1
   type    = "A"
-  zone_id = cloudflare_zone.digraph_me.id
   value   = hcloud_server.webserver.ipv4_address
+  zone_id = data.cloudflare_zones.digraph_me.zones[0].id
 }
