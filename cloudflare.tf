2d5d8055af76402f6c5ff2967cacf6f04089b2c8
Leo Gaskin <leo.gaskin@le0.gs>
2021-09-30 12:03:46 +0200

    Add Google Search identification DNS record

--- cloudflare.tf
+++ cloudflare.tf
@@ -24,3 +24,11 @@ resource "cloudflare_record" "raw_digraph_me" {
   value   = hcloud_server.webserver.ipv4_address
   zone_id = data.cloudflare_zones.digraph_me.zones[0].id
 }
+
+resource "cloudflare_record" "google_search_digraph_me" {
+  name    = "@"
+  ttl     = 1
+  type    = "TXT"
+  value   = "google-site-verification=FfBK-ilG6TZVxXRvPXxuB77Mn9xz8FuJJcBIZYOXgX0"
+  zone_id = data.cloudflare_zones.digraph_me.zones[0].id
+}
