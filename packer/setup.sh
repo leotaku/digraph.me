1bb56b3f8422250314218f993b225599edcd3092
Leo Gaskin <leo.gaskin@le0.gs>
2022-07-07 09:33:46 +0200

    Improve setup script

--- packer/setup.sh
+++ packer/setup.sh
@@ -4,10 +4,13 @@ set -e
 # Install
 export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo apt-key add -
+sudo -E apt-get -y update
+sudo -E apt-get -y install curl gpg
+
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-sudo -E apt-get update
-sudo -E apt-get install caddy
+sudo -E apt-get -y update
+sudo -E apt-get -y install caddy
 
 # Create files
 mkdir -p /var/web
