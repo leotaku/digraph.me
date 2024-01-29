68b59895652a80f724a42fdfcfb110a5419c7bda
Leo Gaskin <leo.gaskin@le0.gs>
2024-01-29 18:26:07 +0100

    Upgrade system packages in setup script

--- image/setup.sh
+++ image/setup.sh
@@ -6,6 +6,7 @@ export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 sudo -E apt-get -y update
 sudo -E apt-get -y install curl gpg
+sudo -E apt-get -y upgrade
 
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
