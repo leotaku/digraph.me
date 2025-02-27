5aef49411038587202467466df7af60e2c361f6e
Leo Gaskin <leo.gaskin@le0.gs>
2025-02-27 08:18:16 +0100

    Also install docker on webserver

--- image/setup.sh
+++ image/setup.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env sh
 set -e
 
-# Install
+# Install core packages
 export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 sudo -E apt-get -y update
 sudo -E apt-get -y install curl gpg
 sudo -E apt-get -y upgrade
 
+# Install caddy
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 sudo -E apt-get -y update
 sudo -E apt-get -y install caddy
+
+# Install docker
+curl -1sLf 'https://download.docker.com/linux/debian/gpg' | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
+echo "deb [signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo -E apt-get -y update
+sudo -E apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Configure unattended upgrades
 sudo -E apt-get -y update
