2839c6fff548332d88775bc9f9e6c6529d3d40d1
Leo Gaskin <leo.gaskin@le0.gs>
2025-04-19 11:10:40 +0200

    Update to Cloudflare provider version 5
    Unfortunately, the new version is still a bit shit. In particular, it
    seems to not recognize when state has not changed. Also for some weird
    reason "automatic_https_rewrites" cannot be set?

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,42 +1,45 @@
 provider "cloudflare" {}
 
 data "cloudflare_zone" "digraph_me" {
-  name = "digraph.me"
+  filter = { name = "digraph.me" }
 }
 
-resource "cloudflare_record" "digraph_me" {
-  name    = "@"
+resource "cloudflare_dns_record" "digraph_me" {
+  name    = "digraph.me"
   proxied = true
   ttl     = 1
   type    = "A"
   content = hcloud_server.webserver.ipv4_address
-  zone_id = data.cloudflare_zone.digraph_me.id
+  zone_id = data.cloudflare_zone.digraph_me.zone_id
 }
 
-resource "cloudflare_record" "raw_digraph_me" {
-  name    = "raw"
+resource "cloudflare_dns_record" "raw_digraph_me" {
+  name    = "raw.digraph.me"
   proxied = false
   ttl     = 1
   type    = "A"
   content = hcloud_server.webserver.ipv4_address
-  zone_id = data.cloudflare_zone.digraph_me.id
+  zone_id = data.cloudflare_zone.digraph_me.zone_id
 }
 
-resource "cloudflare_record" "google_search_digraph_me" {
-  name    = "@"
+resource "cloudflare_dns_record" "google_search_digraph_me" {
+  name    = "digraph.me"
   ttl     = 1
   type    = "TXT"
   content = "google-site-verification=FfBK-ilG6TZVxXRvPXxuB77Mn9xz8FuJJcBIZYOXgX0"
-  zone_id = data.cloudflare_zone.digraph_me.id
+  zone_id = data.cloudflare_zone.digraph_me.zone_id
 }
 
 resource "cloudflare_page_rule" "page_rules_digraph_me" {
-  zone_id  = data.cloudflare_zone.digraph_me.id
+  zone_id  = data.cloudflare_zone.digraph_me.zone_id
   target   = "digraph.me/*"
+  status   = "active"
   priority = 1
 
-  actions {
+  actions = {
     cache_level              = "basic"
     automatic_https_rewrites = "off"
   }
+
+  lifecycle { ignore_changes = [target, actions] }
 }
